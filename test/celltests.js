fe20a8721384e20a1b0ebe91a71dd388f2e9f2ae
matpierangeli <mat.pierangeli@gmail.com>
2016-06-05 23:51:21 +0200

    replace cell constructor with two separated methods

--- test/celltests.js
+++ test/celltests.js
@@ -4,43 +4,43 @@ var cell = require('../lib/cell.js');
 describe('Game of Life', () => {
   describe('Cell creation', () => {
     it('alive cell', () => {
-      assert.equal('alive', cell(true).currentState);
+      assert.equal('alive', cell.createAliveCell().currentState);
     });
     it('dead cell', () => {
-      assert.equal('dead', cell(false).currentState);
+      assert.equal('dead', cell.createDeadCell().currentState);
     });
   });
 
   describe('Dead cell next generation', () => {
     it('Reborn', () => {
-      var aliveCell = cell(false);
-      assert.equal('alive', aliveCell.nextGenerationState([0,1,1,0,0,0,1,0]).currentState);
+      var deadCell = cell.createDeadCell();
+      assert.equal('alive', deadCell.nextGenerationState([0,1,1,0,0,0,1,0]).currentState);
     });
     it('Remains dead', () => {
-      var aliveCell = cell(false);
-      assert.equal('dead', aliveCell.nextGenerationState([0,0,0,0,0,0,0,0]).currentState);
+      var deadCell = cell.createDeadCell();
+      assert.equal('dead', deadCell.nextGenerationState([0,0,0,0,0,0,0,0]).currentState);
     });
     it('Remains dead with too many alive around', () => {
-      var aliveCell = cell(false);
-      assert.equal('dead', aliveCell.nextGenerationState([0,1,1,0,0,1,1,0]).currentState);
+      var deadCell = cell.createDeadCell();
+      assert.equal('dead', deadCell.nextGenerationState([0,1,1,0,0,1,1,0]).currentState);
     });
   });
 
   describe('Alive cell next generation', () => {
     it('Remains live with two neighbours', () => {
-      var aliveCell = cell(true);
+      var aliveCell = cell.createAliveCell();
       assert.equal('alive', aliveCell.nextGenerationState([0,1,1,0,0,0,0,0]).currentState);
     });
     it('Remains live with three neighbours', () => {
-      var aliveCell = cell(true);
+      var aliveCell = cell.createAliveCell();
       assert.equal('alive', aliveCell.nextGenerationState([0,1,1,0,0,0,0,1]).currentState);
     });
     it('Overcrowding', () => {
-      var aliveCell = cell(true);
+      var aliveCell = cell.createAliveCell();
       assert.equal('dead', aliveCell.nextGenerationState([1,1,0,0,1,0,0,1]).currentState);
     });
     it('Underpopulation', () => {
-      var aliveCell = cell(true);
+      var aliveCell = cell.createAliveCell();
       assert.equal('dead', aliveCell.nextGenerationState([0,0,0,0,0,0,0,1]).currentState);
     });
   });
