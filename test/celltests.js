ea7e820d34dd4d8283c3f714e18c772d64fb1839
matpierangeli <mat.pierangeli@gmail.com>
2016-06-09 22:46:01 +0200

    some small refactoring

--- test/celltests.js
+++ test/celltests.js
@@ -1,116 +1,55 @@
 var assert = require('chai').assert;
 var cell = require('../lib/cell.js');
+var alive = require('../lib/cell.js').createAliveCell();
+var dead = require('../lib/cell.js').createDeadCell();
 
 describe('Cell', () => {
   describe('Cell creation', () => {
     it('Alive cell', () => {
-      assert.equal(cell.ALIVE, cell.createAliveCell().currentState);
+      assert.equal(cell.ALIVE, alive.currentState);
     });
     it('Dead cell', () => {
-      assert.equal(cell.DEAD, cell.createDeadCell().currentState);
+      assert.equal(cell.DEAD, dead.currentState);
     });
   });
 
   describe('Dead cell next generation', () => {
     it('Reborn', () => {
-      var deadCell = cell.createDeadCell();
-      var neighbours = [
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var deadCell = dead;
+      var neighbours = [alive, alive, alive, dead, dead, dead, dead, dead];
       assert.equal(cell.ALIVE, deadCell.nextGenerationState(neighbours).currentState);
     });
     it('Remains dead', () => {
-      var deadCell = cell.createDeadCell();
-      var neighbours = [
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var deadCell = dead;
+      var neighbours = [dead, dead, dead, dead, dead, dead, dead, dead];
       assert.equal(cell.DEAD, deadCell.nextGenerationState(neighbours).currentState);
     });
     it('Remains dead with too many alive around', () => {
-      var deadCell = cell.createDeadCell();
-      var neighbours = [
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var deadCell = dead;
+      var neighbours = [alive, alive, alive, alive, dead, dead, dead, dead];
       assert.equal(cell.DEAD, deadCell.nextGenerationState(neighbours).currentState);
     });
   });
 
   describe('Alive cell next generation', () => {
     it('Remains live with two neighbours', () => {
-      var aliveCell = cell.createAliveCell();
-      var neighbours = [
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var aliveCell = alive;
+      var neighbours = [alive, alive, dead, dead, dead, dead, dead, dead];
       assert.equal(cell.ALIVE, aliveCell.nextGenerationState(neighbours).currentState);
     });
     it('Remains live with three neighbours', () => {
-      var aliveCell = cell.createAliveCell();
-      var neighbours = [
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var aliveCell = alive;
+      var neighbours = [alive, alive, alive, dead, dead, dead, dead, dead];
       assert.equal(cell.ALIVE, aliveCell.nextGenerationState(neighbours).currentState);
     });
     it('Overcrowding', () => {
-      var aliveCell = cell.createAliveCell();
-      var neighbours = [
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createAliveCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var aliveCell = alive;
+      var neighbours = [alive, alive, alive, alive, dead, dead, dead, dead];
       assert.equal(cell.DEAD, aliveCell.nextGenerationState(neighbours).currentState);
     });
     it('Underpopulation', () => {
-      var aliveCell = cell.createAliveCell();
-      var neighbours = [
-        cell.createAliveCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell(),
-        cell.createDeadCell()
-      ]
+      var aliveCell = alive;
+      var neighbours = [alive, dead, dead, dead, dead, dead, dead, dead];
       assert.equal(cell.DEAD, aliveCell.nextGenerationState(neighbours).currentState);
     });
   });
--- test/neighbourhoodtests.js
+++ test/neighbourhoodtests.js
@@ -15,7 +15,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,alive],
   	              [alive,xCell,dead],
   	              [dead,alive,alive]];
-  	var expectedNeighbourhood = [dead,alive,alive,alive,dead,dead,alive,alive];
+  	var expectedNeighbourhood = [dead, alive, alive, alive, dead, dead, alive, alive];
   	var actualNeighbourhood = neighbourhood(matrix,1,1);
   	
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -24,7 +24,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[xCell,alive,alive],
   	              [alive,dead,dead],
   	              [dead,alive,alive]];
-  	var expectedNeighbourhood = [dead,dead,dead,dead,alive,dead,alive,dead];
+  	var expectedNeighbourhood = [dead, dead, dead, dead, alive, dead, alive, dead];
   	var actualNeighbourhood = neighbourhood(matrix,0,0);
   	
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -33,7 +33,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,xCell,alive],
   	              [alive,dead,dead],
   	              [dead,alive,alive]];
-  	var expectedNeighbourhood = [dead,dead,dead,dead,alive,alive,dead,dead];
+  	var expectedNeighbourhood = [dead, dead, dead, dead, alive, alive, dead, dead];
   	var actualNeighbourhood = neighbourhood(matrix,0,1);
 
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -43,7 +43,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,xCell],
   	              [alive,dead,dead],
   	              [dead,alive,alive]];
-  	var expectedNeighbourhood = [dead,dead,dead,alive,dead,dead,dead,dead];
+  	var expectedNeighbourhood = [dead, dead, dead, alive, dead, dead, dead, dead];
   	var actualNeighbourhood = neighbourhood(matrix,0,2);
 
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -53,7 +53,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,alive],
   	              [xCell,dead,dead],
   	              [dead,alive,alive]];
-  	var expectedNeighbourhood = [dead,dead,alive,dead,dead,dead,dead,alive];
+  	var expectedNeighbourhood = [dead, dead, alive, dead, dead, dead, dead, alive];
   	var actualNeighbourhood = neighbourhood(matrix,1,0);
   	
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -63,7 +63,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,alive],
   	              [dead,dead,xCell],
   	              [dead,alive,alive]];
-  	var expectedNeighbourhood = [alive,alive,dead,dead,dead,alive,alive,dead];
+  	var expectedNeighbourhood = [alive, alive, dead, dead, dead, alive, alive, dead];
   	var actualNeighbourhood = neighbourhood(matrix,1,2);
 
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -73,7 +73,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,alive],
   	              [dead,dead,alive],
   	              [xCell,alive,alive]];
-  	var expectedNeighbourhood = [dead,dead,dead,dead,alive,dead,dead,dead];
+  	var expectedNeighbourhood = [dead, dead, dead, dead, alive, dead, dead, dead];
   	var actualNeighbourhood = neighbourhood(matrix,2,0);
 
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -83,7 +83,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,alive],
   	              [dead,dead,alive],
   	              [alive,xCell,alive]];
-  	var expectedNeighbourhood = [dead,dead,alive,alive,alive,dead,dead,dead];
+  	var expectedNeighbourhood = [dead, dead, alive, alive, alive, dead, dead, dead];
   	var actualNeighbourhood = neighbourhood(matrix,2,1);
 
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
@@ -93,7 +93,7 @@ describe('Neighbourhood', () => {
   	var matrix = [[dead,alive,alive],
   	              [dead,dead,alive],
   	              [alive,dead,xCell]];
-  	var expectedNeighbourhood = [dead,alive,dead,dead,dead,dead,dead,dead];
+  	var expectedNeighbourhood = [dead, alive, dead, dead, dead, dead, dead, dead];
   	var actualNeighbourhood = neighbourhood(matrix,2,2);
 
     assert.equal(true, neighbourhoodEquality(expectedNeighbourhood, actualNeighbourhood));
